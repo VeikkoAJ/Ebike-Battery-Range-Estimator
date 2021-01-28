8edbbe46b2b833cd2768e41a7b4a6bdf28c501e3
Veikko <veikko.jaaskelainen@aalto.fi>
2021-01-28 10:36:31 +0200

    custom char mapping

---
+++ EbikeBatteryRangeEstimator/CustomChar.c
@@ -0,0 +1,28 @@
+byte CustomTwo[] = {
+        0x00,
+        0x00,
+        0x19,
+        0x15,
+        0x12,
+        0x00,
+        0x00,
+        0x00
+};
+
+
+
+void createCustomChars() {
+
+}
+
+char getCustomChar(char key) {
+    switch (key) {
+        case '0':
+            return 'o';
+        case '1':
+            return 0x2D;
+        case '2':
+
+
+    }
+}
